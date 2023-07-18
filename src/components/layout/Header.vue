0548d1d4319166ccef77d71018845cf592669be9
Jakeom <dev.jakeom@gmail.com>
2023-07-18 08:33:47 -0400

    first commit

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,32 @@
+<template>
+ <div>
+  <b-navbar type="dark" variant="dark">
+    <b-navbar-nav>
+      <b-nav-item to="/">Home</b-nav-item>
+
+      <!-- Navbar dropdowns -->
+      <b-nav-item-dropdown text="Lang" right>
+        <b-dropdown-item href="#">EN</b-dropdown-item>
+        <b-dropdown-item href="#">ES</b-dropdown-item>
+        <b-dropdown-item href="#">RU</b-dropdown-item>
+        <b-dropdown-item href="#">FA</b-dropdown-item>
+      </b-nav-item-dropdown>
+
+      <b-nav-item-dropdown text="User" right>
+        
+        <b-dropdown-item to="/about">Account</b-dropdown-item>
+        <b-dropdown-item href="#">Settings</b-dropdown-item>
+      </b-nav-item-dropdown>
+      <b-nav-item to="/signup">SignUp</b-nav-item>
+      <b-nav-item to="/login">Login</b-nav-item>
+      <b-nav-item to="/">Logout</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <!-- Page content -->
+    <b-container class="mt--8 pb-5">
+      <b-row class="justify-content-center">
+        <b-col lg="5" md="7">
+          <b-form @submit="onSubmit" >
+            <h2 class="text-center">Log in</h2>
+            <b-form-group class="row py-sm-3 mb-0">
+                <b-input id="input-1" v-model="form.email"
+                type="email"
+                placeholder="Enter email"
+                required
+              ></b-input>
+            </b-form-group>
+            <b-form-group class="row pb-3">
+                <b-input
+                id="input-2"
+                v-model="form.password"
+                placeholder="Password"
+                type="password"
+                required
+              ></b-input>
+            </b-form-group>
+            <div class="row pb-3">
+            <b-form-checkbox v-model="form.rememberMe">Remember me</b-form-checkbox>
+            </div>
+                  <div class="pb-3 text-center">
+                    <button type="primary" class="btn btn-primary btn-block" style="width: 100%;">Sign in</button>
+                  </div>
+            
+          </b-form>
+
+          <b-row class="mt-3 text-center">
+            <b-col cols="6">
+              <router-link to="/dashboard" class=""><small>Forgot password?</small></router-link>
+            </b-col>
+            <b-col cols="6" class="text-right">
+              <router-link to="/register" class=""><small>Create new account</small></router-link>
+            </b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+<script>
+  export default{
+    data(){
+      return{
+        form:{
+          email: '',
+          password: '',
+          rememberMe: false
+        }
+      };
+    },
+    methods:{
+      onSubmit(){
+        // this will be called only after form is valid. You can do api call here to login
+        alert(JSON.stringify(this.form))
+      }
+    }
+  };
+</script>
